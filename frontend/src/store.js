f22234e25ca05fe225b1d496ae215a1cece0d796
zZChopperZz <vanspykid85@gmail.com>
2020-11-18 12:58:26 +0700

    Create Cart Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { createStore, combineReducers, applyMiddleware,compose  } from 'redux';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'; 
 import { productListReducer, productDetailReducer } from './reducers/productReducers';
+import {cartReducer} from './reducers/cartReducers';
+import Cookie from 'js-cookie';
 
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+
+const initialState = {cart: {cartItems}};
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailReducer
+    productDetails: productDetailReducer,
+    cart: cartReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
